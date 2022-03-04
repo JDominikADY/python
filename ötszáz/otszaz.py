826c5eb2060b27130b3eb9bb429589bcea8d9f7c
Jenovai Dominik Roland <jenovaid20@ADY-NAGYATAD.SULINET.HU>
2022-03-04 11:25:55 +0100

    3.feladat

--- ötszáz/otszaz.py
+++ ötszáz/otszaz.py
@@ -1,3 +1,41 @@
 
 f=open("penztar.txt")
+kosar=[]
+for sor in f:
+    kosar.append(sor.strip())
+
+
+f.close()
+print("2.felad")
+print("A fizetések száma: " + str(kosar.count("F")))
+
+
+print("3.feladat")
+print("Az első vásárló " + str(kosar.index("F"))+ " darab árucikket vásárolt.")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
