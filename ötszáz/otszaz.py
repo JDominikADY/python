a4c50bb1634fc709afb011f142affe3239ee4461
Jenovai Dominik Roland <jenovaid20@ADY-NAGYATAD.SULINET.HU>
2022-03-07 08:43:19 +0100

    5.feladat

--- ötszáz/otszaz.py
+++ ötszáz/otszaz.py
@@ -1,4 +1,4 @@
-
+#1
 f=open("penztar.txt")
 kosar=[]
 for sor in f:
@@ -6,20 +6,31 @@ for sor in f:
 
 
 f.close()
-print("2.felad")
+
+#2
+print("2.feladta")
 print("A fizetések száma: " + str(kosar.count("F")))
 
 
+#3
 print("3.feladat")
 print("Az első vásárló " + str(kosar.index("F"))+ " darab árucikket vásárolt.")
 
 
 
+#4
+sorszam=int(input("Vásárlás sorszáma: "))
+arunev=input("Árucikk neve: ")
+darab=int(input("Darabszám: "))
 
 
-
-
-
+print("5. feladat")
+aruIndex=kosar.index(arunev)
+darabLista=kosar[:aruIndex]
+print(darabLista)
+vasarlasDb=darabLista.count("F")+ 1
+print(vasarlasDb)
+print("Az első vásárlás sorszáma" + str(vasarlasDb))
 
 
 
