af6b1039a9ecc2439912a891dc5155a071357de5
Jenovai Dominik Roland <jenovaid20@ADY-NAGYATAD.SULINET.HU>
2022-03-21 08:24:49 +0100

    7feladatKESZ

--- ötszáz/otszaz.py
+++ ötszáz/otszaz.py
@@ -1,3 +1,9 @@
+def ertek(darab):
+    if darab==1:
+        return 500
+    else:
+        return darab*400+150
+
 #1
 f=open("penztar.txt")
 kosar=[]
@@ -32,7 +38,54 @@ vasarlasDb=darabLista.count("F")+ 1
 print(vasarlasDb)
 print("Az első vásárlás sorszáma" + str(vasarlasDb))
 
+voltF=False
+szam=0
+for e in kosar:
+    if e==arunev:
+        if not voltF:
+            szam=szam+1
+            voltF=True
+    if e=="F":
+        voltF=False
+print(str(szam) + " vásárlás során vettek belőle")
 
+
+#6
+print("6.feladat")
+print(str(vasarlasDb) + "darab vételekor fizetendő: " + str(ertek(vasarlasDb)))
+
+#7
+print("7.feladat")
+darabF=0
+elozoIndex=0
+keresettIndex=0
+
+for i in range(0, len(kosar)):
+    if kosar[i]=="F":
+        darabF+=1
+        elozoIndex=keresettIndex
+        keresettIndex=i
+        
+    if darabF==sorszam:
+        break
+
+print(kosar[elozoIndex+1:keresettIndex])
+
+if sorszam>1:
+    darabKosar=kosar[elozoIndex+1:keresettIndex]
+else:
+    darabKosar=kosar[elozoIndex:keresettIndex]
+
+stat={}
+for e in darabKosar:
+   if e in stat.keys():
+       stat[e]+=1
+   else:
+       stat[e]=1
+
+#print(stat)
+for e in stat:
+    print(str(stat[e]) + " " + e)
 
 
 
