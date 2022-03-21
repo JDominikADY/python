3507718e7bd725f03bca84419b63145b49f1f1b8
JDominikADY <99969456+JDominikADY@users.noreply.github.com>
2022-03-21 08:45:24 +0100

    Add files via upload

--- ötszáz/otszaz.py
+++ ötszáz/otszaz.py
@@ -87,19 +87,18 @@ for e in darabKosar:
 for e in stat:
     print(str(stat[e]) + " " + e)
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+#8
+stat={}
+ossz=[]
+for e in kosar:
+    if e=="F":
+        osszeg=0
+        for i in stat.values():
+            osszeg+=ertek(i)
+        ossz.append(osszeg)
+        stat={}
+    else:
+        if e in stat.keys():
+            stat[e]+=1
+        else:
+            stat[e]=1
