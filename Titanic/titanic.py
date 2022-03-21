9c1719acd3d9b412f45b6ec794743d8d9ccd9110
Jenovai Dominik Roland <jenovaid20@ADY-NAGYATAD.SULINET.HU>
2022-03-21 12:33:32 +0100

    3FeladatEleje

---
+++ Titanic/titanic.py
@@ -0,0 +1,27 @@
+#1
+f=open("titanic.txt")
+adatok=f.read().split("\n")
+
+f.close()
+
+
+#2
+print("2. feladat: " + str(len(adatok)) + " db")
+
+
+#3
+print("3. feladat: " + str(sum(int(e.split(";")[1]) + int(e.split(";")[2]) for e in adatok)) + " fő")
+
+ossz=0
+for e in adatok:
+    temp=e.split(";")
+    tulelo=int(temp[1])
+    eltunt=int(temp[2])
+    ossz+=tulelo
+    ossz+=eltunt
+print("3. feladat: " + str(ossz) + " fő")
+
+#4
+kulcsSzo=input("4. feladat: Kulcsszó: ")
+van=False
+#find()
