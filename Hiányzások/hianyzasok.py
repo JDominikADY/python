9d716f33cfc29f29b918787d76f08be46d5f3154
Jenovai Dominik Roland <jenovaid20@ADY-NAGYATAD.SULINET.HU>
2022-04-04 12:31:32 +0200

    hianyzottEddig

--- Hiányzások/hianyzasok.py
+++ Hiányzások/hianyzasok.py
@@ -1,3 +1,4 @@
+
 #masodikverzio
 #elsoverzio
 f=open("naplo.txt")
@@ -11,8 +12,8 @@ nap=0
 for e in adatok:
     if e[0]=="#":
         # 01 15
-        honap=e[2:3]
-        nap=e[5:]
+        honap=int(e[2:3])
+        nap=int(e[5:])
         #print(honap)
     else:
         temp=[]
@@ -39,6 +40,73 @@ for e in naplo:
     igazolatlan+=e[3].count("I")
 
 print("3.feladat\nAz igazolt hiányzások száma " + str(igazolt) + ", az igazolatlanoké " + str(igazolatlan) + ".")
+
+
+#4 FELADAT
+def hetnapja(honap,nap):
+    napnev = ("vasarnap", "hetfo", "kedd", "szerda", "csutortok", "pentek", "szombat")
+    napszam = (0, 31, 59, 90, 120, 151, 181, 212, 243, 273, 304, 335)
+    napsorszam = (napszam[honap-1]+ nap) % 7
+    return napnev[napsorszam]
+
+
+#5 FELADAT
+print("5. feladat")
+honap=int(input("A hónap sorszáma= "))
+nap=int(input("a nap sorszáma= "))
+print("Azon a napon " + hetnapja(honap,nap) + " volt.")
+
+
+#6 FELADAT
+print("6.feladat")
+napnev=input("A nap neve: ")
+ora=int(input("Az óra száma: "))
+
+db=0
+for e in naplo:
+    if hetnapja(e[0],e[1])==napnev:
+        if e[3][ora-1]=="X" or e[3][ora-1]=="I":
+            db+=1
+
+print("Ekkor összesen " + str(db) + " óra hiányzás történt.")
+
+db=sum([e[3][ora-1] in ["X","I"] for e in naplo if hetnapja(e[0],e[1])==napnev])
+#print(db)
+
+
+#7 FELADAT
+stat={}
+for e in naplo:
+    if e[2] in stat.keys():
+        stat[e[2]] += e[3].count("X")+e[3].count("I")
+    else:
+        stat[e[2]] = e[3].count("X")+e[3].count("I")
+
+maximum=0
+for e in stat:
+    if stat[e]>maximum:
+        maximum=stat[e]
+
+print("A legtöbbet hiányzó tanulók: ", end="")
+for e in stat:
+    if stat[e]==maximum:
+        print(e,end=" ")
+
+print()
+maximum=max(stat.values())
+leg=[e for e in stat if stat[e]==maximum]
+
+print(leg)
+print("A legtöbbet hiányzó tanulók: " + " ".join(leg))
+
+#print(stat)
+#print(maxi(stat))
+
+
+
+
+
+
 
 
 
