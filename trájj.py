09ff9ed0bf50d8bba90ee2677d6a2d0c77c5ce92
Your Name <you@example.com>
2022-04-26 08:44:45 +0200

    sa

--- trájj.py
+++ trájj.py
@@ -1,3 +1,16 @@
-szam=input("Kérek egy számot: ")
 
-print(szam)
+szam=""
+
+while szam=="":
+    try:
+        szam=int(input("Kérek egy számot: "))
+    except:
+        print("Ez nem szám volt")
+    else:
+        print("De hülye vagy?")
+    finally:
+        print("Megvagyunk!")
+
+print(szam*2)
+
+
