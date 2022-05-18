39150f36249723c0cafacb2424fd7a1d52cb6b6a
Jenovai Dominik Roland <jenovaid20@ADY-NAGYATAD.SULINET.HU>
2022-05-18 09:40:13 +0200

    3feladaat

--- erettsegi/Forrasok/4_Sudoku/sudoku.py
+++ erettsegi/Forrasok/4_Sudoku/sudoku.py
@@ -1,3 +1,5 @@
+import math
+
 #filenev=input("Adja meg a bemeneti fájl nevét: ")
 #sor=input("Adja meg egy sor számát: ")
 #oszlop=input("Adja meg az oszlop számát: ")
@@ -25,7 +27,6 @@ for sor in adatok:
 #print(adatok)
 #print(adatok2)
 
-
 tabla=adatok[:9]
 lepesek=adatok[9:]
 #print(tabla)
@@ -33,4 +34,29 @@ lepesek=adatok[9:]
 
 
 print("3.feladat")
-print("Az adott helyen szereplő szám: " + str(tabla[sor-1][oszlop-1]))
+
+if tabla[sor-1][oszlop-1]=="0":
+    print("Az adott helyen még nem töltötték ki.")
+else:
+    print("Az adott helyen szereplő szám: " + str(tabla[sor-1][oszlop-1]))
+
+
+s=math.ceil(sor/3)-1
+o=math.ceil(oszlop/3)
+
+
+print("A hely a(z) {} résztáblához tartozik.".format(s*3+o))
+
+db=0
+for s in tabla:
+    db+=s.count("0")
+
+print("4.feladat")
+print("Az üres helyek aránya: {:.0}".format(db/81))
+
+for lepes in lepesek:
+    print(lepes)
+
+
+    
+
