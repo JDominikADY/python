953a3e3b9862dffb5005c76ca8abc96f98ad414c
Jenovai Dominik Roland <jenovaid20@ADY-NAGYATAD.SULINET.HU>
2022-05-20 09:40:08 +0200

    nemjomemondig

--- erettsegi/Forrasok/4_Sudoku/sudoku.py
+++ erettsegi/Forrasok/4_Sudoku/sudoku.py
@@ -1,62 +1,88 @@
 import math
 
-#filenev=input("Adja meg a bemeneti fájl nevét: ")
-#sor=input("Adja meg egy sor számát: ")
-#oszlop=input("Adja meg az oszlop számát: ")
+def oszlop_ban_van_e(oszlop,ertek):
+    for o in tabla:
+        if o[oszlop]==ertek:
+            return True
+        return False
+
+        
+#filenev=input("Adja meg a bemeneti fájl nevét!")
+#sor=int(input("Adja meg egy sor számát!"))
+#oszlop=int(input("Adja meg egy oszlop számát!"))
 filenev="nehez.txt"
-sor=1
-oszlop=1
+sor=7
+oszlop=7
 
 f=open(filenev)
 sorok=f.read().split("\n")[:-1]
 f.close()
 
+
 adatok=[]
 for sor in sorok:
     adatok.append(sor.split(" "))
-
 
 adatok2=[]
 for sor in adatok:
     temp=[]
     for elem in sor:
         temp.append(int(elem))
-
     adatok2.append(temp)
-
-#print(adatok)
-#print(adatok2)
+    
+print(adatok)
+print(adatok2)
 
 tabla=adatok[:9]
 lepesek=adatok[9:]
-#print(tabla)
-#print(lepesek)
 
+print("3. feladat")
 
-print("3.feladat")
+#print("Az adott helyen szereplő szám: "+ str(tabla[sor-1][oszlop-1]))
 
-if tabla[sor-1][oszlop-1]=="0":
-    print("Az adott helyen még nem töltötték ki.")
+if tabla[sor-1][oszlop-1]==0:
+    print("Adott helyet még nem töltötték ki.")
 else:
-    print("Az adott helyen szereplő szám: " + str(tabla[sor-1][oszlop-1]))
-
-
+    print("Az adott helyen szereplő szám: "+str(tabla[sor-1][oszlop-1]))
 s=math.ceil(sor/3)-1
 o=math.ceil(oszlop/3)
 
+tabla=s*3+o
 
-print("A hely a(z) {} résztáblához tartozik.".format(s*3+o))
+print("A hely a(z) {} résztáblázathoz tartozik.".format(s*3+o))
 
 db=0
 for s in tabla:
     db+=s.count("0")
-
-print("4.feladat")
-print("Az üres helyek aránya: {:.0}".format(db/81))
+print("4. feladat")
+print("Az üres helyek aránya: {:.0%}".format(db/81))
 
 for lepes in lepesek:
-    print(lepes)
+    t_s=int(lepes[1])-1 #sor
+    t_o=int(lepes[2])-1 #oszlop
+    print(tabla[int(lepes[1])-1][int(lepes[2])-1])
+
+    print(lepes[0] in tabla[int(lepes[1])-1])
+    if tabla[t_s][t_o]!="0":
+        print(tabla[int(lepes[1])-1][int(lepes[2])-1])
+        print("A helyet már kitöltötték.")
+    elif lepes[0] in tabla[t_s]:
+        pass
+    elif oszlop_ban_van_e(t_o,lepes[0]):
+        
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
     
-
