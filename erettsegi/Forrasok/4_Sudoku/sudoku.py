3b68c1337cbeeb1732d9090bac518c6a8ac3f209
Jenovai Dominik Roland <jenovaid20@ADY-NAGYATAD.SULINET.HU>
2022-05-23 12:33:04 +0200

    hamarba

--- erettsegi/Forrasok/4_Sudoku/sudoku.py
+++ erettsegi/Forrasok/4_Sudoku/sudoku.py
@@ -6,6 +6,13 @@ def oszlop_ban_van_e(oszlop,ertek):
             return True
         return False
 
+def resztabla(sor,oszlop):
+    s=math.ceil((sor+1)/3)-1
+    o=math.ceil((oszlop+1)/3)
+
+    return s*3+o
+    
+    
         
 #filenev=input("Adja meg a bemeneti fájl nevét!")
 #sor=int(input("Adja meg egy sor számát!"))
@@ -44,10 +51,12 @@ if tabla[sor-1][oszlop-1]==0:
     print("Adott helyet még nem töltötték ki.")
 else:
     print("Az adott helyen szereplő szám: "+str(tabla[sor-1][oszlop-1]))
+
+
 s=math.ceil(sor/3)-1
 o=math.ceil(oszlop/3)
 
-tabla=s*3+o
+
 
 print("A hely a(z) {} résztáblázathoz tartozik.".format(s*3+o))
 
@@ -57,20 +66,38 @@ for s in tabla:
 print("4. feladat")
 print("Az üres helyek aránya: {:.0%}".format(db/81))
 
+
+print("5. feladat")
 for lepes in lepesek:
     t_s=int(lepes[1])-1 #sor
     t_o=int(lepes[2])-1 #oszlop
+
+    print(resztabla(t_s,t_o))
+
+    volt=False
+    for i in range(0,9):
+        for k in range(0,9):
+            if resztabla(t_s,t_o)==resztabla(i,k):
+                if tabla[i][k]==lepes[0]:
+                    volt=True
+                    
+    
+    print(lepes)
     print(tabla[int(lepes[1])-1][int(lepes[2])-1])
 
     print(lepes[0] in tabla[int(lepes[1])-1])
+    
     if tabla[t_s][t_o]!="0":
-        print(tabla[int(lepes[1])-1][int(lepes[2])-1])
         print("A helyet már kitöltötték.")
     elif lepes[0] in tabla[t_s]:
-        pass
+        print("Az adott sorban már szerepel a szám.")
     elif oszlop_ban_van_e(t_o,lepes[0]):
+        print("Az adott oszlopban már szerepel a szám.")
+    elif volt:
+        print("Az adott résztáblázatban szerepel a szám")
+    else:
+        print("A lépés megtehető")
         
-
 
 
 
