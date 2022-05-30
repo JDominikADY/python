ffdb5f99a388612b826a6ced21b575176be2cb65
Jenovai Dominik Roland <jenovaid20@ADY-NAGYATAD.SULINET.HU>
2022-05-30 08:44:41 +0200

    sajt

---
+++ sms/smsmodul.py
@@ -0,0 +1,10 @@
+class adatok:
+    def __init__(self, sor1, sor2):
+        vag=sort1.split(" ")
+        self.ora=int(vag[0])
+        self.perc=int(vag[1])
+        self.telefonszam=vag[2]
+
+        self.uzenet=sor2
+        
+
